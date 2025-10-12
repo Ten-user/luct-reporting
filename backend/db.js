7188d0763abbc66a2dc6e70543f9d621bf312c40
Ten-user <tentlaba13@gmail.com>
2025-10-12 15:49:47 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,8 @@
-// backend/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
